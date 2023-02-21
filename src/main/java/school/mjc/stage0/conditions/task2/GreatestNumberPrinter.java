799fb4b086f451b575984e384ab85314eff5261b
PavelDenisov <pavel.den111@gmail.com>
2023-02-21 15:50:58 +0200

    done

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        if (first > second){
+            System.out.println(first);
+        }
+        else if (second > first) {
+            System.out.println(second);
+        }
+        else {
+            System.out.println(second);
+        }
     }
 }
+//1. Greatest number printer.
+//        Implement the program that will find the greatest number from two that are passed
+//        as parameters to the method and print the greatest.
+//        In case of equality print any of them. Implement program in the code snippet:
